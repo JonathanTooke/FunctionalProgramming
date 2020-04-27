094db3c52b86b9ed6c0ba92d8f1ab665d0db4f8b
Jonathan Tooke <jonathantooke@gmail.com>
2020-04-27 13:27:03 +0200

    Updated to include Accept and Reject states

---
+++ README.txt
@@ -0,0 +1,12 @@
+Sequence of Turing machine state configurations that are used during the computation of f with the input string: aba:
+
+< q0, 'a', ' ', q1 >
+< q1, 'b', 'b', q1 >
+< q1, 'a', 'a', q1 >
+< q1, ' ', ' ', q3 >
+< q3, 'a', ' ', q5 >
+< q5, 'b', 'b', q5 >
+< q5, ' ', ' ', q1 >
+< q1, 'b', ' ', q2 >
+< q2, ' ', ' ', q4 >
+< q4, ' ', ' ', qAccept>
--- partC.hs
+++ partC.hs
@@ -1,3 +1,11 @@
+
+-- Palindrome Function --
+turingPalindrome :: [Char] -> Char
+turingPalindrome (xs)   | length (xs) > 4               = qReject                                                       --reject any string with length greater tahn 4
+                        | (xs) == ""                    = qAccept                                                       --accept the empty string
+                        | inTMAlphabet (xs) == False    = qReject                                                       --reject any strings not from the alphabet {'a','b'}
+                        | otherwise                     = q0 (Tape (repeat ' ') (head xs) (tail xs ++ repeat ' '))      --compute the palindrome
+
 -- Tape Declaration --
 data Tape a = Tape [a] a [a]
 
@@ -12,33 +20,30 @@ write :: Tape a -> a -> Tape a
 write (Tape xs y zs) n = Tape xs n zs
 
 -- State Functions --
-q0 (Tape xs y zs)   | y == 'a'  = q1 (moveRight (write (Tape xs y zs) ' '))   --transition to state q1
-                    | y == 'b'  = q2 (moveRight (write (Tape xs y zs) ' '))   --transition to state q2
-                    | y == ' '  = 'a'                                         --accept the palindrome
+q0 (Tape xs y zs)   | y == 'a'  = q1 (moveRight (write (Tape xs y zs) ' '))     --transition to state q1
+                    | y == 'b'  = q2 (moveRight (write (Tape xs y zs) ' '))     --transition to state q2
+                    | y == ' '  = qAccept                                       --accept the palindrome
 
-q1 (Tape xs y zs)   | y == ' '  = q3 (moveLeft (Tape xs y zs))                --reached end of tape, move left and transition to q3
-                    | otherwise = q1 (moveRight (Tape xs y zs))               --move right until end of tape
+q1 (Tape xs y zs)   | y == ' '  = q3 (moveLeft (Tape xs y zs))                  --reached end of tape, move left and transition to q3
+                    | otherwise = q1 (moveRight (Tape xs y zs))                 --move right until end of tape
 
-q2 (Tape xs y zs)   | y == ' '  = q4 (moveLeft (Tape xs y zs))                --reached end of tape, move left and transition to q4
-                    | otherwise = q2 (moveRight (Tape xs y zs))               --move right until end of tape
+q2 (Tape xs y zs)   | y == ' '  = q4 (moveLeft (Tape xs y zs))                  --reached end of tape, move left and transition to q4
+                    | otherwise = q2 (moveRight (Tape xs y zs))                 --move right until end of tape
 
-q3 (Tape xs y zs)   | y == 'b' = 'b'                                          --start character not same as end character, reject the palindrome
-                    | y == ' ' = 'a'                                          --no characters left, accept the palindrome
-                    | y == 'a' = q5 (moveLeft (write (Tape xs y zs) ' '))     --start character the same as end character, transition to q5
+q3 (Tape xs y zs)   | y == 'b' = qReject                                        --start character not same as end character, reject the palindrome
+                    | y == ' ' = qAccept                                        --no characters left, accept the palindrome
+                    | y == 'a' = q5 (moveLeft (write (Tape xs y zs) ' '))       --start character the same as end character, transition to q5
 
-q4 (Tape xs y zs)   | y == 'a' = 'b'                                          --start character not same as end character, reject the palindrome
-                    | y == ' ' = 'a'                                          --no characters left, accept the palindrome
-                    | y == 'b' = q5 (moveLeft (write (Tape xs y zs) ' '))     --start character the same as end character, transition to q5
+q4 (Tape xs y zs)   | y == 'a' = qReject                                        --start character not same as end character, reject the palindrome
+                    | y == ' ' = qAccept                                        --no characters left, accept the palindrome
+                    | y == 'b' = q5 (moveLeft (write (Tape xs y zs) ' '))       --start character the same as end character, transition to q5
 
-q5 (Tape xs y zs)   | y == ' '  = q0 (moveRight (Tape xs y zs))               --check if it is a blank (i.e. far left of tape), if so move right and transition to q0.
-                    | otherwise = q5 (moveLeft (Tape xs y zs))                --otherwise, keep moving left
+q5 (Tape xs y zs)   | y == ' '  = q0 (moveRight (Tape xs y zs))                 --check if it is a blank (i.e. far left of tape), if so move right and transition to q0.
+                    | otherwise = q5 (moveLeft (Tape xs y zs))                  --otherwise, keep moving left
 
--- Palindrome Function --
-turingPalindrome :: [Char] -> Char
-turingPalindrome (xs)   | length (xs) > 4               = 'b'                                                           --reject any string with length greater tahn 4
-                        | (xs) == ""                    = 'a'                                                           --accept the empty string
-                        | inTMAlphabet (xs) == False    = 'b'                                                           --reject any strings not from the alphabet {'a','b'}
-                        | otherwise                     = q0 (Tape (repeat ' ') (head xs) (tail xs ++ repeat ' '))      --compute the palindrome
+qAccept = 'a'
+
+qReject = 'b'
 
 -- Check to see if a string only contains the characters 'a' and 'b' --
 inTMAlphabet :: [Char] -> Bool
